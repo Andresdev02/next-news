9ec6b8f5514942f023217268ddf4584079cf7d82
Andreske02 <andres.vergauwen@hotmail.com>
2021-11-25 16:01:28 +0100

    refactor(index): add head to home

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,21 @@
+import Head from 'next/head';
 import {Toolbar} from '../components/toolbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-<main className='page-container'>
-  <Toolbar />
-  <article className={styles.main}>
-    <h1>Next.js News App</h1>
-    <h3>Your one stop shop for the latest news articles</h3>
-  </article>
-</main>
+    <>  
+    <Head>
+    <meta name="viewport" content="initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"/>
+        <title>News app | Home</title>
+      </Head>
+    <main className='page-container'>
+      <Toolbar />
+      <article className={styles.main}>
+        <h1>Next.js News App</h1>
+        <h3>Your one stop shop for the latest news articles</h3>
+      </article>
+    </main>
+</>
   )
 }
